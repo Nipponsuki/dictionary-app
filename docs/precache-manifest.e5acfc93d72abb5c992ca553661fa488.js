dd12940721f81572633822bd94015d07e63da90a
Alex <alex.alexeev23061991@gmail.com>
2019-02-12 10:33:11 +0300

    first commit

---
+++ docs/precache-manifest.e5acfc93d72abb5c992ca553661fa488.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "de82329ce7b0c2e47f1d",
+    "url": "/static/js/main.de82329c.chunk.js"
+  },
+  {
+    "revision": "dfc5214818a6a0433c00",
+    "url": "/static/js/1.dfc52148.chunk.js"
+  },
+  {
+    "revision": "de82329ce7b0c2e47f1d",
+    "url": "/static/css/main.29cfb636.chunk.css"
+  },
+  {
+    "revision": "dd7e5f1d8cce8e571546750a777ab490",
+    "url": "/index.html"
+  }
+];
